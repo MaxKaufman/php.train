e903f84ea2f5a1a508b561bffeaa7ba2fb95b557
Alex <susl1k123@mail.ru>
2018-06-24 17:51:29 +0300

    создал таблицу, вывожу ошибки

--- test2.php
+++ test2.php
@@ -2,7 +2,14 @@
 const DBNAME = 'my.db';
 $db = new SQLite3(DBNAME);
 
-
+$db->exec("CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  age INTEGER  
+)");
+if ($db->lastErrorCode() > 0) {
+    echo $db->lastErrorCode() . " " . $db->lastErrorMsg();
+}
 
 
 unset($db);
